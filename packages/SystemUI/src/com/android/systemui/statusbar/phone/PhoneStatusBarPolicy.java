4a8ea32d24d027013a21459453b7c4af9a5c40b5
Bai Haojie <bai.haojie@zte.com.cn>
2022-08-02 18:27:00 +0800

    Fix no icon display for alarm clock
    After upgrading the data not erased, the user sets the alarm clock,
    but the alarm clock icon is not displayed, and the alarm clock can
    start normally.
    
    Change-Id: Ibc669a19eea93d55050f2a6d413135f1fbb1eb2d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -216,6 +216,7 @@ public class PhoneStatusBarPolicy
         mDisplayId = displayId;
         mSharedPreferences = sharedPreferences;
         mDateFormatUtil = dateFormatUtil;
+        onUserSetupChanged();
     }
 
     /** Initialize the object after construction. */
