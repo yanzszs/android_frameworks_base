776b02e71bccfe48f489c2dbe363fd85173b6084
str143 <alexfinhart@gmail.com>
2022-08-30 09:52:59 +0800

    Volume Media output dialog when stream is active toggle [1/2]
    Signed-off-by: str143 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5578,6 +5578,11 @@ public final class Settings {
          */
         public static final String QS_WEATHER_POSITION = "qs_weather_position";
         
+	/**
+         * @hide
+         */
+        public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
