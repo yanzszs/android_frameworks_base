5e3915bd066737a64ace92818d53d684bebf9eea
spkal01 <kalligeross@gmail.com>
2022-09-19 18:18:36 +0800

    base: Settings UI customizations
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5766,6 +5766,18 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_DATE_FORMAT = "status_bar_clock_date_format";
 
         /**
+         * Settings Dashboard Style
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
+	/**
+         * Settings Homepage Layout
+         * @hide
+         */
+        public static final String USE_STOCK_LAYOUT = "use_stock_layout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
