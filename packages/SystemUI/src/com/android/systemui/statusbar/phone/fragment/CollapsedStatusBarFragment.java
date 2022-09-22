SageOfD6Path/corvus_android_frameworks_base

Statusbar Clock: Reduce background pill paddings
Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -740,9 +740,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 
         if (mShowSBClockBg) {
             mClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mClockView.setPadding(10,5,10,5);
+            mClockView.setPadding(10,2,10,2);
             mRightClock.setBackgroundResource(R.drawable.sb_date_bg);
-            mRightClock.setPadding(10,5,10,5);
+            mRightClock.setPadding(10,2,10,2);
         } else {
             mClockView.setBackgroundResource(0);
             mClockView.setPadding(0,0,0,0);

yanzszs/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -769,11 +769,11 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
         if (mShowSBClockBg) {
             mClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mClockView.setPadding(10,5,10,5);
+            mClockView.setPadding(10,2,10,2);
             mCenterClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mCenterClockView.setPadding(10,5,10,5);
+            mCenterClockView.setPadding(10,2,10,2);
             mRightClockView.setBackgroundResource(R.drawable.sb_date_bg);
-            mRightClockView.setPadding(10,5,10,5);
+            mRightClockView.setPadding(10,2,10,2);
         } else {
             mClockView.setBackgroundResource(0);
             mClockView.setPadding(0,0,0,0);