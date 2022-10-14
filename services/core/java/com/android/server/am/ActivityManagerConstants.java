aosp-mirror/platform_frameworks_base

commit 1a4e911f53b4f554915a69289c7e2e0a5ad9f00c

ActivityManagerConstants: enable skipping MCP kills until BOOT_COMPLETE
Delaying MCP kills seems good overall, so enable it by default with a
ten minute timeout.

Test: TH
bug: 222365734
Change-Id: I0822b182e6a0e736b22ca944f9e4be8514fd8c7d

services/core/java/com/android/server/am/ActivityManagerConstants.java
@@ -668,11 +668,11 @@ final class ActivityManagerConstants extends ContentObserver {
             "no_kill_cached_processes_post_boot_completed_duration_millis";
 
     /** @see #mNoKillCachedProcessesUntilBootCompleted */
-    private static final boolean DEFAULT_NO_KILL_CACHED_PROCESSES_UNTIL_BOOT_COMPLETED = false;
+    private static final boolean DEFAULT_NO_KILL_CACHED_PROCESSES_UNTIL_BOOT_COMPLETED = true;
 
     /** @see #mNoKillCachedProcessesPostBootCompletedDurationMillis */
     private static final long
-            DEFAULT_NO_KILL_CACHED_PROCESSES_POST_BOOT_COMPLETED_DURATION_MILLIS = 0;
+            DEFAULT_NO_KILL_CACHED_PROCESSES_POST_BOOT_COMPLETED_DURATION_MILLIS = 600_000;
 
     /**
      * If true, do not kill excessive cached processes proactively, until user-0 is unlocked.

yanzszs/android_frameworks_base

commit 715cff4c6471c6bfdb5c9983eada78a1584e4fd5

services: Kill cached processes at startup
* kernels on custom rom community have different memory killers like simple lmk, tng, legacy lmk,etc.
  however this patch imo is intended for lmkd (which is being nuked on most
  custom kernels in favor of custom lmks), to lessen the hassle
  caused by commit 1a4e911f53b4f554915a69289c7e2e0a5ad9f00c , restore the legacy behaviour.

this commit reverts 1a4e911f53b4f554915a69289c7e2e0a5ad9f00c

Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

services/core/java/com/android/server/am/ActivityManagerConstants.java
@@ -729,7 +729,7 @@ final class ActivityManagerConstants extends ContentObserver {
             "no_kill_cached_processes_post_boot_completed_duration_millis";
 
     /** @see #mNoKillCachedProcessesUntilBootCompleted */
-    private static final boolean DEFAULT_NO_KILL_CACHED_PROCESSES_UNTIL_BOOT_COMPLETED = true;
+    private static final boolean DEFAULT_NO_KILL_CACHED_PROCESSES_UNTIL_BOOT_COMPLETED = false;
 
     /** @see #mNoKillCachedProcessesPostBootCompletedDurationMillis */
     private static final long